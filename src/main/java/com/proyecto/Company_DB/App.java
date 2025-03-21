e572350b2e7b328c4cf99d0704537908971833fc
Alvaro <azarbea179@g.educaand.es>
2025-03-21 09:07:59 +0100

    Primer Commit

---
+++ src/main/java/com/proyecto/Company_DB/App.java
@@ -0,0 +1,13 @@
+package com.proyecto.Company_DB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
