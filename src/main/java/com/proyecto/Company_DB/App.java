537dc26fbec7e3323956b676277d1306f59d3fbe
Alvaro <azarbea179@g.educaand.es>
2025-03-21 10:40:12 +0100

    Nuevos cambios

--- src/main/java/com/proyecto/Company_DB/App.java
+++ src/main/java/com/proyecto/Company_DB/App.java
@@ -1,13 +1,33 @@
 package com.proyecto.Company_DB;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+	public static void main(String[] args) {
+		String url = "jdbc:oracle:thin:@//localhost:1521/XE";
+		String user = "company_db";
+		String password = "company";
+
+		
+		try (Connection conn = DriverManager.getConnection(url, user, password);
+				Statement st = conn.createStatement();
+				ResultSet rs = st.executeQuery("SELECT * FROM employees order by EMPLOYEE_ID asc")) {
+
+			
+			while (rs.next()) {
+				int id = rs.getInt("EMPLOYEE_ID");
+				String nombre = rs.getString("FIRST_NAME");
+		
+
+				System.out.println("ID: " + id + ", Nombre: " + nombre);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Error de conexión: " + e.getMessage());
+		}
+	}
 }
