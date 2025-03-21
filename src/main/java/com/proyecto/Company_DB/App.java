15cb81b1fea77c067f5f66d3a8016e40ebc0f01e
Alvaro <azarbea179@g.educaand.es>
2025-03-21 14:24:13 +0100

    Nuevos cambios

--- src/main/java/com/proyecto/Company_DB/App.java
+++ src/main/java/com/proyecto/Company_DB/App.java
@@ -1,10 +1,7 @@
 package com.proyecto.Company_DB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
 
 public class App {
 	public static void main(String[] args) {
@@ -12,22 +9,19 @@ public class App {
 		String user = "company_db";
 		String password = "company";
 
-		
 		try (Connection conn = DriverManager.getConnection(url, user, password);
 				Statement st = conn.createStatement();
 				ResultSet rs = st.executeQuery("SELECT * FROM employees order by EMPLOYEE_ID asc")) {
 
-			
 			while (rs.next()) {
 				int id = rs.getInt("EMPLOYEE_ID");
 				String nombre = rs.getString("FIRST_NAME");
-		
-
-				System.out.println("ID: " + id + ", Nombre: " + nombre);
+				String email = rs.getString("email");
+				System.out.println("ID: " + id + ", Nombre: " + nombre + ", Email: " + email);
 			}
-
 		} catch (SQLException e) {
 			System.out.println("Error de conexión: " + e.getMessage());
 		}
 	}
 }
+
