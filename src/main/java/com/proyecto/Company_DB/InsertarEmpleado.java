8d3fbc840850c49defddfc2b566ea8a93e4312f2
Alvaro <azarbea179@g.educaand.es>
2025-03-27 10:26:58 +0100

    Corregido fallo: No insertaba los usuarios en la base de datos

--- src/main/java/com/proyecto/Company_DB/InsertarEmpleado.java
+++ src/main/java/com/proyecto/Company_DB/InsertarEmpleado.java
@@ -9,7 +9,7 @@ public class InsertarEmpleado {
 	public static void main(String[] args) {
 
 		Connection conn = null;
-		Statement st = null;
+		PreparedStatement st = null;
 
 		Scanner sc = new Scanner(System.in);
 
@@ -29,9 +29,18 @@ public class InsertarEmpleado {
 
 		try {
 			conn = DBUtils.getConnection();
-			st = conn.createStatement();
-			int numFilas = st.executeUpdate(
+			st = conn.prepareStatement(
 					"INSERT INTO EMPLOYEES (FIRST_NAME, LAST_NAME, EMAIL, PHONE, HIRE_DATE, JOB_TITLE) VALUES (?, ?, ?, ?, ?, ?)");
+
+			st.setString(1, nombre);
+			st.setString(2, apellido);
+			st.setString(3, email);
+			st.setString(4, telefono);
+			st.setString(5, fecha_alta);
+			st.setString(6, job_title);
+			
+			int numFilas = st.executeUpdate();
+			
 
 			System.out.println("Se han insertado " + numFilas + " filas");
 		} catch (SQLException e) {
