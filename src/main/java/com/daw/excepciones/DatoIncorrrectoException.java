b3f68485bbfcc5276e78f3c6f5d773754d0ef09b
Alvaro <azarbea179@g.educaand.es>
2025-04-10 10:13:30 +0200

    Ventanas Creadas

--- src/main/java/com/daw/excepciones/DatoIncorrrectoException.java
+++ src/main/java/com/daw/excepciones/DatoIncorrrectoException.java
@@ -2,11 +2,6 @@ package com.daw.excepciones;
 
 public class DatoIncorrrectoException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 	public DatoIncorrrectoException(String msg) {
 		super(msg);
 	}
